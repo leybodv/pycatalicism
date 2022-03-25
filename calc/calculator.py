9a1c91fd40378da0e71e61dd552cfc389c2aa696
leybodv <leybodv@gmail.com>
2022-03-25 17:04:02 +0300

    new file:   calc/calculator.py

---
+++ calc/calculator.py
@@ -0,0 +1,8 @@
+class Calculator():
+    """
+    """
+
+    def calculate(self, output_data_path:str, show_plot:bool, output_plot_path:str):
+        """
+        """
+        raise NotImplementedError()
