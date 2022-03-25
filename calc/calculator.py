c7295af9ba73dd2fe23eb5bde844642b4c7f1c29
leybodv <leybodv@gmail.com>
2022-03-25 18:31:23 +0300

    calculate: started adding implementation

--- calc/calculator.py
+++ calc/calculator.py
@@ -5,4 +5,11 @@ class Calculator():
     def calculate(self, output_data_path:str, show_plot:bool, output_plot_path:str):
         """
         """
-        raise NotImplementedError()
+        input_data = data_importer.get_input_data()
+        conversion = data_calculator.calculate_conversion(input_data)
+        selectivity = data_calculator.calculate_selectivity(input_data)
+        self._print_results(conversion, selectivity)
+        if output_data_path is not None:
+            self._export_results(output_data_path, conversion, selectivity)
+        if show_plot or output_plot_path is not None:
+            plotter.plot(conversion, selectivity, show_plot, output_plot_path)
