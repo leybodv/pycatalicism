dd58cf2f331d024a4a3226b1e50036cdc645f1b5
leybodv <leybodv@gmail.com>
2022-03-25 15:08:16 +0300

    calculate:added implementation

--- pycat.py
+++ pycat.py
@@ -3,6 +3,7 @@
 import logging
 import logging_config
 import argparse
+import calc
 
 logger = logging.getLogger(__name__)
 logging_config.configure_logger(logger)
@@ -11,16 +12,17 @@ def calculate(args:argparse.Namespace):
     """
     """
     logger.info('Calculating conversion and selectivity vs. temperature')
-    raise NotImplementedError()
+    calculator = calc.main.get_calculator(args.reaction)
+    calculator.calculate(args.output_data, args.show_plot, args.output_plot)
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(required=True)
-calc = subparsers.add_parser('calc', help='calculate conversion and selectivity vs. temperature')
-calc.set_defaults(func=calculate)
-calc.add_argument('reaction', choices=['co-oxidation', 'co2-hydrogenation'], help='reaction for which to calculate data')
-calc.add_argument('--output-data', help='path to file to save calculated data')
-calc.add_argument('--show-plot', action='store_true', help='whether to show data plot or not')
-calc.add_argument('--output-plot', help='path to file to save plot')
+calc_parser = subparsers.add_parser('calc', help='calculate conversion and selectivity vs. temperature')
+calc_parser.set_defaults(func=calculate)
+calc_parser.add_argument('reaction', choices=['co-oxidation', 'co2-hydrogenation'], help='reaction for which to calculate data')
+calc_parser.add_argument('--output-data', default=None, help='path to file to save calculated data')
+calc_parser.add_argument('--show-plot', action='store_true', help='whether to show data plot or not')
+calc_parser.add_argument('--output-plot', default=None, help='path to file to save plot')
 
 args = parser.parse_args()
 args.func(args)
