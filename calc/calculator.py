a4de0a97632e0e35da635b6141d70f9bba70beb7
leybodv <leybodv@gmail.com>
2022-03-26 16:36:39 +0300

    calculate_activity: added method

--- calc/calculator.py
+++ calc/calculator.py
@@ -7,12 +7,17 @@ class Calculator():
         """
         self.calculator = calculator_factory.get_calculator(reaction)
 
-    def calculate_conversion(self, input_data):
+    def calculate_conversion(self, input_data:RawData):
         """
         """
         raise NotImplementedError()
 
-    def calculate_selecctivity(self, input_data):
+    def calculate_selectivity(self, input_data):
+        """
+        """
+        raise NotImplementedError()
+
+    def calculate_activity(self, input_data):
         """
         """
         raise NotImplementedError()
