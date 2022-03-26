6920f4ef41cb57b6279d5ec55df4d832a9556aba
leybodv <leybodv@gmail.com>
2022-03-26 17:50:19 +0300

    calculate: remade implementation

--- pycat.py
+++ pycat.py
@@ -3,7 +3,7 @@
 import logging
 import logging_config as lc
 import argparse
-import calc.main
+import calc.calc as calc
 
 logger = logging.getLogger(__name__)
 lc.configure_logger(logger)
@@ -12,13 +12,13 @@ def calculate(args:argparse.Namespace):
     """
     """
     logger.info('Calculating conversion and selectivity vs. temperature')
-    calculator = calc.main.get_calculator(args.reaction)
-    calculator.calculate(args.output_data, args.show_plot, args.output_plot)
+    calc.calculate(input_data_path=args.input_data_path, reaction=args.reaction, output_data_path=args.output_data, show_plot=args.show_plot, output_plot_path=args.output_plot)
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(required=True)
 calc_parser = subparsers.add_parser('calc', help='calculate conversion and selectivity vs. temperature')
 calc_parser.set_defaults(func=calculate)
+calc_parser.add_argument('input-data-path', help='path to folder with files from concentration measurement device')
 calc_parser.add_argument('reaction', choices=['co-oxidation', 'co2-hydrogenation'], help='reaction for which to calculate data')
 calc_parser.add_argument('--output-data', default=None, help='path to file to save calculated data')
 calc_parser.add_argument('--show-plot', action='store_true', help='whether to show data plot or not')
