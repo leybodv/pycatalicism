22e499cb049acfc8a442eeb02197746eba1b0321
leybodv <leybodv@gmail.com>
2022-03-28 21:29:18 +0300

    don't use config path in parser_factory

--- calc/parser_factory.py
+++ calc/parser_factory.py
@@ -1,5 +1,4 @@
 import logging
-from pathlib import Path
 
 from calc import logging_config
 from calc.parser import Parser
@@ -7,9 +6,7 @@ from calc.parser import Parser
 logger = logging.getLogger(__name__)
 logging_config.configure_logger(logger)
 
-CONFIG_PATH = Path('calc.cfg')
-
-def get_parser(config_path:Path=CONFIG_PATH) -> Parser:
+def get_parser(parser_type:str) -> Parser:
     """
     """
     raise NotImplementedError()
