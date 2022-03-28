4f2091bc2034739ebc6f423a014a14816541233b
leybodv <leybodv@gmail.com>
2022-03-28 21:42:03 +0300

    new file:   calc/cooxidationcalculator.py

---
+++ calc/cooxidationcalculator.py
@@ -0,0 +1,15 @@
+import logging
+
+from calc import logging_config
+from calc.calculator import Calculator
+
+class COOxidationCalculator(Calculator):
+    """
+    """
+
+    def __init__(self):
+        """
+        """
+        self.logger = logging.getLogger(__class__.__name__)
+        logging_config.configure_logger(self.logger)
+        raise NotImplementedError()
