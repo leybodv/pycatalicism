e690f6eeee0c19cd1a37daf26bdb55e1a698f91d
leybodv <leybodv@gmail.com>
2022-03-28 23:13:02 +0300

    added raw_data_parser_type parameter

---
+++ config.py
@@ -0,0 +1,10 @@
+# parser type used to convert raw data from equipment for gas composition measurement to input data for calculator to calculate conversion/activity/selectivity vs. temperature
+#
+# chromatec-crystal-composition-copy-paste
+#   raw data obtained via copy/paste of composition calculation results from chromatec analytics software
+#   data must be in a format:
+#   Температура<tab><temperature>
+#   <br>
+#   Название<tab>Время, мин<tab>Детектор<tab>Концентрация<tab>Ед, измерения<tab>Площадь<tab>Высота
+#   <compound-name><tab><retention-time><tab><detector-name><tab><compound-concentration><tab><concentration-units><tab><peak-area><tab><peak-height>
+raw_data_parser_type = 'chromatec-crystal-composition-copy-paste'
