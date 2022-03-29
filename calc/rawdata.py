52efd1c89cc12844f5d9deb8a20e8bde65485523
leybodv <leybodv@gmail.com>
2022-03-29 23:11:27 +0300

    get_temperatures: added method

--- calc/rawdata.py
+++ calc/rawdata.py
@@ -3,7 +3,13 @@ class RawData():
     Wrapper for imported data storage
     """
 
-    def __init__(self):
+    def __init__(self, temperatures:list[float]):
         """
         """
+        self.temperatures = temperatures
         raise NotImplementedError()
+
+    def get_temperatures(self) -> list[float]:
+        """
+        """
+        return self.temperatures
