4728ad952fa365eddb6c57beee5c4462b90d5d40
leybodv <leybodv@gmail.com>
2022-03-29 23:13:50 +0300

    get_init_amb_temp: added method

--- calc/rawdata.py
+++ calc/rawdata.py
@@ -3,13 +3,19 @@ class RawData():
     Wrapper for imported data storage
     """
 
-    def __init__(self, temperatures:list[float]):
+    def __init__(self, temperatures:list[float], initial_ambient_temperature:float|None=None):
         """
         """
         self.temperatures = temperatures
+        self.init_amb_temp = initial_ambient_temperature
         raise NotImplementedError()
 
     def get_temperatures(self) -> list[float]:
         """
         """
         return self.temperatures
+
+    def get_init_amb_temp(self) -> float|None:
+        """
+        """
+        return self.init_amb_temp
