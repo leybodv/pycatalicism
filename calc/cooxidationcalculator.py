21509195e7477980347c24162ec4a4443e66ef12
leybodv <leybodv@gmail.com>
2022-03-29 19:47:34 +0300

    calculate_conversion: added implementation

--- calc/cooxidationcalculator.py
+++ calc/cooxidationcalculator.py
@@ -2,6 +2,8 @@ import logging
 
 from . import logging_config
 from .calculator import Calculator
+from .rawdata import RawData
+from .conversion import Conversion
 
 class COOxidationCalculator(Calculator):
     """
@@ -17,5 +19,27 @@ class COOxidationCalculator(Calculator):
     def calculate_conversion(self, input_data:RawData) -> Conversion:
         """
         """
-        self.logger.debug(f'calculating conversion')
-        raise NotImplementedError()
+        self.logger.debug(f'calculatong conversion')
+        temperatures = []
+        alphas = []
+        for temperature in input_data.get_temperature():
+            T_i = input_data.get_init_amb_temp()
+            p_i = input_data.get_init_amb_pres()
+            f_i = input_data.get_init_flow()
+            T_f = input_data.get_fin_amb_temp(temperature)
+            p_f = input_data.get_fin_amb_pres(temperature)
+            f_f = input_data.get_fin_flow(temperature)
+            C_CO_i = input_data.get_init_conc('CO')
+            C_CO_f = input_data.get_conc('CO', temperature)
+            if T_i == None or p_i == None or f_i == None or T_f == None or p_f == None or f_f == None:
+                T_i = 1
+                p_i = 1
+                f_i = 1
+                T_f = 1
+                p_f = 1
+                f_f = 1
+            alpha = (C_CO_i - C_CO_f) / C_CO_i
+            temperatures.append(temperature)
+            alphas.append(alpha)
+        conversion = Conversion(temperatures, alphas)
+        return conversion
