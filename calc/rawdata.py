f9fe1390de1a48a2903b4697a1360876c0a8b086
leybodv <leybodv@gmail.com>
2022-03-30 00:04:38 +0300

    added getters for instance variables

--- calc/rawdata.py
+++ calc/rawdata.py
@@ -1,17 +1,23 @@
+import numpy as np
+
 class RawData():
     """
     Wrapper for imported data storage
     """
 
-    def __init__(self, temperatures:list[float], initial_ambient_temperature:float|None=None, initial_ambient_pressure:float|None=None):
+    def __init__(self, temperatures:list[float]|np.ndarray[float,np.dtype], initial_ambient_temperature:float|None=None, initial_ambient_pressure:float|None=None, initial_flow:float|None=None, final_ambient_temperatures:list[float]|np.ndarray[float,np.dtype]|None=None, final_ambient_pressures:list[float]|np.ndarray[float,np.dtype]|None=None, final_flows:list[float]|np.ndarray[float,np.dtype]|None=None):
         """
         """
-        self.temperatures = temperatures
+        self.temperatures = np.array(temperatures)
         self.init_amb_temp = initial_ambient_temperature
         self.init_amb_pres = initial_ambient_pressure
+        self.init_flow = initial_flow
+        self.fin_amb_temps = None if final_ambient_temperatures is None else np.array(final_ambient_temperatures)
+        self.fin_amb_pres = None if final_ambient_pressures is None else np.array(final_ambient_pressures)
+        self.fin_flows = None if final_flows is None else np.array(final_flows)
         raise NotImplementedError()
 
-    def get_temperatures(self) -> list[float]:
+    def get_temperatures(self) -> np.ndarray[float, np.dtype]:
         """
         """
         return self.temperatures
@@ -25,3 +31,32 @@ class RawData():
         """
         """
         return self.init_amb_pres
+
+    def get_init_flow(self) -> float|None:
+        """
+        """
+        return self.init_flow
+
+    def get_fin_amb_temp(self, temperature:float) -> float|None:
+        """
+        """
+        if self.fin_amb_temps is not None:
+            return float(self.fin_amb_temps[self.temperatures == temperature])
+        else:
+            return None
+
+    def get_fin_amb_pres(self, temperature:float) -> float|None:
+        """
+        """
+        if self.fin_amb_pres is not None:
+            return float(self.fin_amb_pres[self.temperatures == temperature])
+        else:
+            return None
+
+    def get_fin_flow(self, temperature:float) -> float|None:
+        """
+        """
+        if self.fin_flows is not None:
+            return float(self.fin_flows[self.temperatures == temperature])
+        else:
+            return None
