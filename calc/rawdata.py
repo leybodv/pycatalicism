493cbb3e7f0d44ddc31e374c5a14dabc754273ed
leybodv <leybodv@gmail.com>
2022-03-29 23:14:57 +0300

    get_init_amb_pres: added method

--- calc/rawdata.py
+++ calc/rawdata.py
@@ -3,11 +3,12 @@ class RawData():
     Wrapper for imported data storage
     """
 
-    def __init__(self, temperatures:list[float], initial_ambient_temperature:float|None=None):
+    def __init__(self, temperatures:list[float], initial_ambient_temperature:float|None=None, initial_ambient_pressure:float|None=None):
         """
         """
         self.temperatures = temperatures
         self.init_amb_temp = initial_ambient_temperature
+        self.init_amb_pres = initial_ambient_pressure
         raise NotImplementedError()
 
     def get_temperatures(self) -> list[float]:
@@ -19,3 +20,8 @@ class RawData():
         """
         """
         return self.init_amb_temp
+
+    def get_init_amb_pres(self) -> float|None:
+        """
+        """
+        return self.init_amb_pres
