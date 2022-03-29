13bb498ddfbee90636dce286d0ff205058e10d26
leybodv <leybodv@gmail.com>
2022-03-29 19:24:00 +0300

    parse_data: adding implementation

--- calc/chromatec_crystal_composition_copy_paste_parser.py
+++ calc/chromatec_crystal_composition_copy_paste_parser.py
@@ -1,7 +1,9 @@
 import logging
+from pathlib import Path
 
 from . import logging_config
 from .parser import Parser
+from .rawdata import RawData
 
 class ChromatecCrystalCompositionCopyPasteParser(Parser):
     """
@@ -13,3 +15,10 @@ class ChromatecCrystalCompositionCopyPasteParser(Parser):
         self.logger = logging.getLogger(__class__.__name__)
         logging_config.configure_logger(self.logger)
         self.logger.debug(f'creating {__class__.__name__}')
+
+    def parse_data(self, input_data_path:Path) -> RawData:
+        """
+        """
+        self.logger.debug(f'parsing data from {input_data_path}')
+        raise NotImplementedError()
+        return rawdata
