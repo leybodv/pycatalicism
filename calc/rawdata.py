868b64efa19f7636157047bf8ba478cfc139967e
leybodv <leybodv@gmail.com>
2022-03-30 18:25:11 +0300

    get_init_conc: added method

--- calc/rawdata.py
+++ calc/rawdata.py
@@ -5,7 +5,7 @@ class RawData():
     Wrapper for imported data storage
     """
 
-    def __init__(self, temperatures:list[float]|np.ndarray[float,np.dtype], initial_ambient_temperature:float|None=None, initial_ambient_pressure:float|None=None, initial_flow:float|None=None, final_ambient_temperatures:list[float]|np.ndarray[float,np.dtype]|None=None, final_ambient_pressures:list[float]|np.ndarray[float,np.dtype]|None=None, final_flows:list[float]|np.ndarray[float,np.dtype]|None=None):
+    def __init__(self, temperatures:list[float]|np.ndarray[float,np.dtype], initial_concentrations:dict[str,float], initial_ambient_temperature:float|None=None, initial_ambient_pressure:float|None=None, initial_flow:float|None=None, final_ambient_temperatures:list[float]|np.ndarray[float,np.dtype]|None=None, final_ambient_pressures:list[float]|np.ndarray[float,np.dtype]|None=None, final_flows:list[float]|np.ndarray[float,np.dtype]|None=None):
         """
         """
         self.temperatures = np.array(temperatures)
@@ -15,6 +15,7 @@ class RawData():
         self.fin_amb_temps = None if final_ambient_temperatures is None else np.array(final_ambient_temperatures)
         self.fin_amb_pres = None if final_ambient_pressures is None else np.array(final_ambient_pressures)
         self.fin_flows = None if final_flows is None else np.array(final_flows)
+        self.init_concs = initial_concentrations
         raise NotImplementedError()
 
     def get_temperatures(self) -> np.ndarray[float, np.dtype]:
@@ -60,3 +61,8 @@ class RawData():
             return float(self.fin_flows[self.temperatures == temperature])
         else:
             return None
+
+    def get_init_conc(self, compound:str) -> float:
+        """
+        """
+        return self.init_concs['CO']
