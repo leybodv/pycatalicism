5fa7d8c73e43e927cfc5a60cf0fe315cd94b5ab4
leybodv <leybodv@gmail.com>
2022-03-30 19:13:18 +0300

    __init__: added implementation

--- calc/conversion.py
+++ calc/conversion.py
@@ -1,9 +1,12 @@
+import numpy as np
+
 class Conversion():
     """
     Wrapper for conversion data storage
     """
 
-    def __init__(self):
+    def __init__(self, temperatures:list[float], alphas:list[float]):
         """
         """
-        raise NotImplementedError()
+        self.temperatures = np.array(temperatures)
+        self.alphas = np.array(alphas)
