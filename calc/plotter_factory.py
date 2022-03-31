f8d628a1f3d8d624bcd1b8d7a5f33953e412be9c
leybodv <leybodv@gmail.com>
2022-03-31 13:06:05 +0300

    get_plotter: added implementation

--- calc/plotter_factory.py
+++ calc/plotter_factory.py
@@ -1,6 +1,11 @@
 from .plotter import Plotter
+from .co_oxidation_plotter import COOxidationPlotter
+from .plotterexception import PlotterException
 
 def get_plotter(reaction:str) -> Plotter:
     """
     """
-    raise NotImplementedError()
+    if reaction == 'co-oxidation':
+        return COOxidationPlotter()
+    else:
+        raise PlotterException(f'Cannot create plotter for reaction "{reaction}"')
