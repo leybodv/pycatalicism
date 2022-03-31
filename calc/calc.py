babfcb68c1f5c73691c2f575b79cf4136fea85f9
leybodv <leybodv@gmail.com>
2022-03-31 10:40:27 +0300

    calculate: change paths parameters' type hinting from Path to str

--- calc/calc.py
+++ calc/calc.py
@@ -21,17 +21,17 @@ def _print_results(conversion, selectivity):
     print(conversion)
     print(selectivity)
 
-def calculate(input_data_path:Path, initial_data_path:Path, reaction:str, parser_type:str, output_data_path:Path|None=None, show_plot:bool=False, output_plot_path:Path|None=None):
+def calculate(input_data_path:str, initial_data_path:str, reaction:str, parser_type:str, output_data_path:str|None=None, show_plot:bool=False, output_plot_path:str|None=None):
     """
     """
     logger.debug(f'calculating conversion and selectivity for reaction {reaction}')
     calculator = calculator_factory.get_calculator(reaction)
     parser = parser_factory.get_parser(parser_type)
-    input_data = parser.parse_data(input_data_path, initial_data_path)
+    input_data = parser.parse_data(Path(input_data_path), Path(initial_data_path))
     conversion = calculator.calculate_conversion(input_data)
     selectivity = calculator.calculate_selectivity(input_data)
     _print_results(conversion, selectivity)
     if output_data_path is not None:
-        _export_results(output_data_path, conversion, selectivity)
+        _export_results(Path(output_data_path), conversion, selectivity)
     if show_plot or (output_plot_path is not None):
-        plotter.plot(conversion, selectivity, show_plot, output_plot_path)
+        plotter.plot(conversion, selectivity, show_plot, Path(output_plot_path))
