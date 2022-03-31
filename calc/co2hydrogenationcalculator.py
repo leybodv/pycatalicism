ec9f714cd7ea47526e31e9e831cf36f779b9c8a3
leybodv <leybodv@gmail.com>
2022-03-31 18:28:01 +0300

    removed logger

--- calc/co2hydrogenationcalculator.py
+++ calc/co2hydrogenationcalculator.py
@@ -1,6 +1,3 @@
-import logging
-
-from . import logging_config
 from .calculator import Calculator
 
 class CO2HydrogenationCalculator(Calculator):
@@ -10,6 +7,4 @@ class CO2HydrogenationCalculator(Calculator):
     def __init__(self):
         """
         """
-        self.logger = logging.getLogger(__class__.__name__)
-        logging_config.configure_logger(self.logger)
         raise NotImplementedError()
