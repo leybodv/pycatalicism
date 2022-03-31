283053db42365fd1b644f0b77fc9bdd43fb445b4
leybodv <leybodv@gmail.com>
2022-03-31 12:02:15 +0300

    removed logger

--- calc/exporter_factory.py
+++ calc/exporter_factory.py
@@ -1,10 +1,4 @@
-import logging
-
-from . import logging_config
 from .exporter import Exporter
-
-logger = logging.getLogger(__name__)
-logging_config.configure_logger(logger)
 
 def get_exporter(reaction:str) -> Exporter:
     """
