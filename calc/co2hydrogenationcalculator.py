3d6d5135e7276632a6650102d87c229f8336f35c
leybodv <leybodv@gmail.com>
2022-03-31 18:30:15 +0300

    calculate_conversion, calculate_selectivity: added methods

--- calc/co2hydrogenationcalculator.py
+++ calc/co2hydrogenationcalculator.py
@@ -1,10 +1,23 @@
 from .calculator import Calculator
+from .rawdata import RawData
+from .conversion import Conversion
+from .selectivity import Selectivity
 
 class CO2HydrogenationCalculator(Calculator):
     """
     """
 
     def __init__(self):
+        """
+        """
+        raise NotImplementedError()
+
+    def calculate_conversion(self, input_data:RawData) -> Conversion:
+        """
+        """
+        raise NotImplementedError()
+
+    def calculate_selectivity(self, input_data:RawData) -> Selectivity:
         """
         """
         raise NotImplementedError()
