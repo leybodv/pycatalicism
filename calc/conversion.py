1fb3443491b2b08284d4891ecc568f19579f0d5f
leybodv <leybodv@gmail.com>
2022-03-31 11:37:02 +0300

    __str__: sort data before converting to str

--- calc/conversion.py
+++ calc/conversion.py
@@ -15,6 +15,26 @@ class Conversion():
         """
         """
         string = 'Temperature\tConversion\n'
-        for temperature, alpha in zip(self.temperatures, self.alphas):
+        sorted_conversion = self.get_sorted()
+        for temperature, alpha in zip(sorted_conversion.get_temperatures(), sorted_conversion.get_alphas()):
             string = string + f'{temperature}\t{alpha}\n'
         return string
+
+    def get_sorted(self) -> 'Conversion':
+        """
+        """
+        zipped_lists = zip(self.temperatures, self.alphas, strict=True)
+        sorted_pairs = sorted(zipped_lists)
+        tuples = zip(*sorted_pairs)
+        sorted_temperatures, sorted_alphas = [list(tuple) for tuple in tuples]
+        return Conversion(sorted_temperatures, sorted_alphas)
+
+    def get_temperatures(self) -> np.ndarray[float, np.dtype]:
+        """
+        """
+        return self.temperatures
+
+    def get_alphas(self) -> np.ndarray[float, np.dtype]:
+        """
+        """
+        return self.alphas
