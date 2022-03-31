5151d111824228748f240b8d2907f51d08a924a2
leybodv <leybodv@gmail.com>
2022-03-31 12:38:20 +0300

    added logging

--- calc/co_oxidation_exporter.py
+++ calc/co_oxidation_exporter.py
@@ -1,13 +1,21 @@
 from pathlib import Path
+import logging
 
 from .exporter import Exporter
 from .conversion import Conversion
 from .selectivity import Selectivity
 from .exporterexception import ExporterException
+from . import logging_config
 
 class COOxidationExporter(Exporter):
     """
     """
+
+    def __init__(self):
+        """
+        """
+        self.logger = logging.getLogger(__class__.__name__)
+        logging_config.configure_logger(self.logger)
 
     def export(self, output_data_path:Path, conversion:Conversion, selectivity:Selectivity|None):
         """
@@ -19,6 +27,7 @@ class COOxidationExporter(Exporter):
     def _export_conversion(self, output_data_path:Path, conversion:Conversion):
         """
         """
+        self.logger.info(f'Exporting conversion vs. temperature data for CO oxidation reaction to "{output_data_path.joinpath("conversion.dat")}"')
         if not output_data_path.exists():
             output_data_path.mkdir(parents=True)
         with output_data_path.joinpath('conversion.dat').open(mode='w') as f:
