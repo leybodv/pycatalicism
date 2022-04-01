e03371e838a4e88ccd16b03ef69bd6b0e9dd0567
leybodv <leybodv@gmail.com>
2022-04-01 22:09:25 +0300

    get_plotter: added CO2HydrogenationPlotter

--- calc/plotter_factory.py
+++ calc/plotter_factory.py
@@ -1,5 +1,6 @@
 from .plotter import Plotter
 from .co_oxidation_plotter import COOxidationPlotter
+from .co2_hydrogenation_plotter import CO2HydrogenationPlotter
 from .plotterexception import PlotterException
 
 def get_plotter(reaction:str) -> Plotter:
@@ -7,5 +8,7 @@ def get_plotter(reaction:str) -> Plotter:
     """
     if reaction == 'co-oxidation':
         return COOxidationPlotter()
+    elif reaction == 'co2-hydrogenation':
+        return CO2HydrogenationPlotter()
     else:
         raise PlotterException(f'Cannot create plotter for reaction "{reaction}"')
