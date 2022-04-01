4a63032ed87b8ac7d9c14a25170fc6517c069409
leybodv <leybodv@gmail.com>
2022-04-01 18:07:50 +0300

    get_exporter: return CO2HydrogenationExporter for corresponding reaction

--- calc/exporter_factory.py
+++ calc/exporter_factory.py
@@ -1,4 +1,5 @@
 from .exporter import Exporter
+from .co2_hydrogenation_exporter import CO2HydrogenationExporter
 from .co_oxidation_exporter import COOxidationExporter
 from .exporterexception import ExporterException
 
@@ -7,5 +8,7 @@ def get_exporter(reaction:str) -> Exporter:
     """
     if reaction == 'co-oxidation':
         return COOxidationExporter()
+    elif reaction == 'co2-hydrogenation':
+        return CO2HydrogenationExporter()
     else:
         raise ExporterException(f'Cannot create exporter for reaction "{reaction}"')
