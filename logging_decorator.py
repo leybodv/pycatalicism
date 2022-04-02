9292f1ea25014cb6ee685416ecfffe61a49949b5
leybodv <leybodv@gmail.com>
2022-04-02 19:57:22 +0300

    new file:   logging_decorator.py

---
+++ logging_decorator.py
@@ -0,0 +1,35 @@
+import functools
+import inspect
+import logging
+
+from . import config
+
+class Logging:
+
+    def __init__(self, func):
+        """
+        """
+        functools.update_wrapper(self, func)
+        self.func = func
+        self.logging_levels = config.logging_levels
+
+    def __call__(self, *args, **kwargs):
+        """
+        """
+        if inspect.isfunction(self.func):
+            logger = logging.getLogger(self.func.__module__)
+            self._configure_logger(logger)
+            raise NotImplementedError()
+        elif self.func.__name__ == '__init__' or inspect.ismethod(self.func):
+            obj = self.func.__self__
+            obj.logger = logging.getLogger(obj.__class__.__name__)
+            self._configure_logger(obj.logger)
+            raise NotImplementedError()
+        else:
+            raise Exception(f'Cannot decorate function {self.func.__name__}')
+        return self.func(*args, **kwargs)
+
+    def _configure_logger(self, logger:logging.Logger):
+        """
+        """
+        raise NotImplementedError()
