e40096fdbfee3760400d2dddcdf9e108e311b12c
leybodv <leybodv@gmail.com>
2022-04-10 18:53:03 +0300

    added docs

--- calc/calc.py
+++ calc/calc.py
@@ -4,16 +4,48 @@ from pycatalicism.calc import calculator_factory
 from pycatalicism.calc import parser_factory
 from pycatalicism.calc import exporter_factory
 from pycatalicism.calc import plotter_factory
+from pycatalicism.calc.conversion import Conversion
+from pycatalicism.calc.selectivity import Selectivity
+
+"""
+Main interface for calculating conversion, activity and selectivity of catalysts. Users of this module should use calculate method to perform all calculation tasks.
+"""
 
 
-def _print_results(conversion, selectivity):
+def _print_results(conversion:Conversion, selectivity:Selectivity):
     """
+    Print results for conversion and selectivities vs. temperature to console.
+
+    parameters
+    ----------
+    conversion:Conversion
+        Reactant conversions at different temperatures data wrapper
+    selectivity:Selectivity
+        Catalyst selectivities for different product components at different temperatures data wrapper
     """
     print(conversion)
     print(selectivity)
 
 def calculate(input_data_path:str, initial_data_path:str, reaction:str, parser_type:str, output_data_path:str|None=None, show_plot:bool=False, output_plot_path:str|None=None):
     """
+    Main interface to module. Parses input data from equipment capable of measuring composition and, ideally, initial and final gas total flow rate. Calculates conversion and selectivity data from input data. Prints results to console. If output_data_path was provided exports results. If show_plot is True, shows resulting plots. If output_plot_path was provided, exports corresponding plots.
+
+    parameters
+    ----------
+    input_data_path:str
+        Path to directory with input data files
+    initial_data_path:str
+        Path to file with gas composition data without catalyst (i.e. no reaction occured)
+    reaction:str {co-oxidation|co2-hydrogenation}
+        Chemical reaction to calculate data for
+    parser_type:str {chromatec-crystal-composition-copy-paste}
+        Parser type to use for parsing input data
+    output_data_path:str|None {default:None}
+        Path to directory to export results in text format
+    show_plot:bool {default:False}
+        Whether to show resulting plot
+    output_plot_path:str|None {default:None}
+        Path to directory to export resulting plot
     """
     calculator = calculator_factory.get_calculator(reaction)
     parser = parser_factory.get_parser(parser_type)
