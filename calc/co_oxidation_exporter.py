ca135e10d0a4cff982ba209e847ac741d5ec911c
leybodv <leybodv@gmail.com>
2022-04-10 21:19:49 +0300

    added docs

--- calc/co_oxidation_exporter.py
+++ calc/co_oxidation_exporter.py
@@ -8,16 +8,33 @@ from pycatalicism.logging_decorator import Logging
 
 class COOxidationExporter(Exporter):
     """
+    Class for exporting conversion data for CO oxidation reaction to conversion.dat to a directory provided by user of this class.
     """
 
     @Logging
     def __init__(self):
         """
+        Registers logger with instances of this class which can be accessed via self.logger instance variable.
         """
         super().__init__()
 
     def export(self, output_data_path:Path, conversion:Conversion, selectivity:Selectivity|None):
         """
+        Main interface of this class. Exports conversion data for CO oxidation reaction to conversion.dat to a directory provided by user of this method.
+
+        parameters
+        ----------
+        output_data_path:Path
+            path to directory to export resulting data
+        conversion:Conversion
+            wrapper of CO conversion at different temperatures
+        selectivity:Selectivity|None
+            should be None since selectivity does not make sense for CO oxidation reaction
+
+        raises
+        ------
+        exception:ExporterException
+            if ouput_data_path exists and not directory
         """
         if output_data_path.exists() and not output_data_path.is_dir():
             raise ExporterException(f'Data path for exporting data must be a folder')
@@ -25,6 +42,14 @@ class COOxidationExporter(Exporter):
 
     def _export_conversion(self, output_data_path:Path, conversion:Conversion):
         """
+        Export CO conversion data to conversion.dat at output_data_path directory
+
+        parameters
+        ----------
+        ouput_data_path:Path
+            path to directory to export conversion data
+        conversion:Conversion
+            wrapper of CO conversion at different temperatures
         """
         self.logger.info(f'Exporting conversion vs. temperature data for CO oxidation reaction to "{output_data_path.joinpath("conversion.dat")}"')
         if not output_data_path.exists():
