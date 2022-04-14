e8d5a429809618256edc6ade83fad053c362ebbc
leybodv <leybodv@gmail.com>
2022-04-14 11:21:17 +0300

    calculate_conversion: added method

--- calc/co2hydrogenationproductsbasiscalculator.py
+++ calc/co2hydrogenationproductsbasiscalculator.py
@@ -1,10 +1,17 @@
-from pycatalicism.calc.calculator import Calculator
+from pycatalicism.calc.co2hydrogenationcalculator import CO2HydrogenationCalculator
+from pycatalicism.calc.rawdata import RawData
+from pycatalicism.calc.conversion import Conversion
 
-class CO2HydrogenationProductsBasisCalculator(Calculator):
+class CO2HydrogenationProductsBasisCalculator(CO2HydrogenationCalculator):
     """
     """
 
     def __init__(self):
+        """
+        """
+        raise NotImplementedError()
+
+    def calculate_conversion(self, input_data:RawData) -> Conversion:
         """
         """
         raise NotImplementedError()
