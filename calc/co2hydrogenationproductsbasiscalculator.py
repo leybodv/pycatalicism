0b6521ca33d9ad1b0ec3b40dee7144a9e5169817
leybodv <leybodv@gmail.com>
2022-04-14 11:17:57 +0300

    new file:   calc/co2hydrogenationproductsbasiscalculator.py

---
+++ calc/co2hydrogenationproductsbasiscalculator.py
@@ -0,0 +1,10 @@
+from pycatalicism.calc.calculator import Calculator
+
+class CO2HydrogenationProductsBasisCalculator(Calculator):
+    """
+    """
+
+    def __init__(self):
+        """
+        """
+        raise NotImplementedError()
