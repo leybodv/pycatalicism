72fb6b73414099bc166f9475dd7eb6da451dee47
leybodv <leybodv@gmail.com>
2022-05-23 23:11:06 +0300

    heat: added function

---
+++ furnace/furnace_module.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+def heat(temperature:str|int, wait:str|int|None=None, show_plot:bool=False, export_plot:str|Path|None=None, export_data:str|Path|None=None):
+    """
+    """
+    raise NotImplementedError()
