009ae5bce021d97941bf58568b71b9ebe0ad03ef
leybodv <leybodv@gmail.com>
2022-05-30 20:28:58 +0300

    new file:   furnace/plotter.py

---
+++ furnace/plotter.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+from pycatalicism.furnace.furnace_data import FurnaceData
+
+class Plotter(ABC):
+    """
+    """
+
+    @abstractmethod
+    def plot(self, data:FurnaceData):
+        """
+        """
+        pass
+
+    @abstractmethod
+    def export_plot(self, path:Path):
+        """
+        """
+        pass
