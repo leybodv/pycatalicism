2a1ed781b2efe0fdea5f5e8c6a0e533c74b93184
leybodv <leybodv@gmail.com>
2022-05-30 20:09:29 +0300

    get_controller: added implementation

--- furnace/controller_factory.py
+++ furnace/controller_factory.py
@@ -1,6 +1,22 @@
 from pycatalicism.furnace.controller import Controller
+from pycatalicism.furnace.owen_tpm101_controller import Owen_TPM101_Controller
+from pycatalicism.furnace.furnace_exception import FurnaceException
 
 def get_controller(controller_type:str) -> Controller:
     """
+    Get furnace controller object of corresponding type
+
+    parameters
+    ----------
+    controller_type:str
+        type of furnace controller
+
+    returns
+    -------
+    controller:Controller
+        furnace controller object
     """
-    raise NotImplementedError()
+    if controller_type == 'Owen_TPM101':
+        return Owen_TPM101_Controller()
+    else:
+        raise FurnaceException(f'Unknown controller type {controller_type}')
