bf6240bb7f2aa3991b79b9d89dd852c268fbb487
leybodv <leybodv@gmail.com>
2022-05-31 10:45:16 +0300

    __init__, _handshake: added methods

--- furnace/controller.py
+++ furnace/controller.py
@@ -1,13 +1,26 @@
 from abc import ABC, abstractmethod
 
 from pycatalicism.furnace.furnace_data import FurnaceData
+from pycatalicism.furnace.furnace_exception import FurnaceException
 
 class Controller(ABC):
     """
     """
 
+    def __init__(self):
+        """
+        """
+        if not self._handshake():
+            raise FurnaceException('Cannot connect to furnace controller')
+
     @abstractmethod
     def heat(self, temperature:int, wait:int|None) -> FurnaceData:
+        """
+        """
+        pass
+
+    @abstractmethod
+    def _handshake(self) -> bool:
         """
         """
         pass
