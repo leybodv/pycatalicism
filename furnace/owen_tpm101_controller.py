6936a8482019440e04f4c36b8dba546f1584aebf
leybodv <leybodv@gmail.com>
2022-05-31 11:00:21 +0300

    _handshake: added method

--- furnace/owen_tpm101_controller.py
+++ furnace/owen_tpm101_controller.py
@@ -5,7 +5,17 @@ class Owen_TPM101_Controller(Controller):
     """
     """
 
+    def __init__(self):
+        """
+        """
+        super().__init__()
+
     def heat(self, temperature:int, wait:int|None) -> FurnaceData:
+        """
+        """
+        raise NotImplementedError()
+
+    def _handshake(self) -> bool:
         """
         """
         raise NotImplementedError()
