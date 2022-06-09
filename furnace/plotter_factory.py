c9f43aeb436ee1fafa7782f46842a4b4cd1aa3b7
leybodv <leybodv@gmail.com>
2022-06-09 18:32:19 +0300

    get_plotter: added implementation

--- furnace/plotter_factory.py
+++ furnace/plotter_factory.py
@@ -3,4 +3,7 @@ from pycatalicism.furnace.plotter import Plotter
 def get_plotter(plotter_type:str) -> Plotter:
     """
     """
-    raise NotImplementedError()
+    if plotter_type == 'simple_temp_time_plotter':
+        return SimpleTempTimePlotter()
+    else:
+        raise PlotterException(f'Unknown plotter type: {plotter_type}')
