2ec31569d96286520052db65f3fc53bf0cc51cc0
leybodv <leybodv@gmail.com>
2022-06-10 20:36:14 +0300

    plot, export_plot: added methods

--- furnace/simple_temp_time_plotter.py
+++ furnace/simple_temp_time_plotter.py
@@ -1,10 +1,23 @@
+from pathlib import Path
+
 from pycatalicism.furnace.plotter import Plotter
+from pycatalicism.furnace.furnace_data import FurnaceData
 
 class SimpleTempTimePlotter(Plotter):
     """
     """
 
     def __init__(self):
+        """
+        """
+        raise NotImplementedError()
+
+    def plot(self, data:FurnaceData):
+        """
+        """
+        raise NotImplementedError()
+
+    def export_plot(self, path:Path):
         """
         """
         raise NotImplementedError()
