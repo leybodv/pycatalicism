4e0f37fa5a4b14b75a2f20dc1d16a4dcb90e3ee3
leybodv <leybodv@gmail.com>
2022-06-10 21:38:06 +0300

    __str__: added method

--- furnace/furnace_data.py
+++ furnace/furnace_data.py
@@ -8,6 +8,14 @@ class FurnaceData():
         self.times = times
         self.temperatures = temperatures
 
+    def __str__(self) -> str:
+        """
+        """
+        string = 'Time, min\tTemperature, °C\n'
+        for time, temperature in zip(self.times, self.temperatures):
+            string = string + f'{time}\t{temperature}\n'
+        return string
+
     def get_times(self) -> list[float]:
         """
         """
