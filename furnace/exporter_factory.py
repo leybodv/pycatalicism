d97aa92a1aa8cfe68b665ec57ca387ba2b4eae56
leybodv <leybodv@gmail.com>
2022-06-11 12:57:07 +0300

    added docs

--- furnace/exporter_factory.py
+++ furnace/exporter_factory.py
@@ -1,9 +1,31 @@
+"""
+Module creates concrete implementations of Exporter class using exporter type parameter
+"""
+
 from pycatalicism.furnace.exporter import Exporter
 from pycatalicism.furnace.simple_temp_time_exporter import SimpleTempTimeExporter
 from pycatalicism.furnace.exporter_exception import ExporterException
 
 def get_exporter(exporter_type:str) -> Exporter:
     """
+    Create exporter of corresponding type.
+
+    parameters
+    ----------
+    exporter_type:str
+        Type of exporter to use. Supported types:
+            "simple_temp_time_exporter"
+                Exports data in a format defined in __str__ method of FurnaceData class
+
+    returns
+    -------
+    exporter:Exporter
+        Object responsible for export of time-temperature dependence data
+
+    raises
+    ------
+    ExporterException
+        If inknown exporter type was provided to method
     """
     if exporter_type == 'simple_temp_time_exporter':
         return SimpleTempTimeExporter()
