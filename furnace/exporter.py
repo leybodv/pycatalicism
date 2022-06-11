6ff79e8f6dfd55695a60a2dd67203123829e26bf
leybodv <leybodv@gmail.com>
2022-06-11 12:27:29 +0300

    added docs

--- furnace/exporter.py
+++ furnace/exporter.py
@@ -5,10 +5,19 @@ from pycatalicism.furnace.furnace_data import FurnaceData
 
 class Exporter(ABC):
     """
+    Abstract class representing objects responsible for export of time-temperature data. Concrete subclasses must implement export_data method.
     """
 
     @abstractmethod
     def export_data(self, data:FurnaceData, path:Path):
         """
+        Export data to path provided as parameter to method. Format of exported data is defined by concrete implementation.
+
+        parameters
+        ----------
+        data:FurnaceData
+            Wrapper of time-temperature data
+        path:Path
+            Path to file to export data to
         """
         pass
