5af444d0f757fe950f4dee2a5041b9db59c5d09b
leybodv <leybodv@gmail.com>
2022-06-13 19:36:21 +0300

    added logging levels

--- furnace/furnace_config.py
+++ furnace/furnace_config.py
@@ -1,1 +1,7 @@
-logging_levels = {}
+import logging
+logging_levels = {
+                'FurnaceData'               :    logging.INFO,
+                'Owen_TPM101_Controller'    :    logging.INFO,
+                'SimpleTempTimeExporter'    :    logging.INFO,
+                'SimpleTempTimePlotter'     :    logging.INFO
+                }
