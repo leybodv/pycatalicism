2b31f43239f4f025d734651ddb1504859f20f487
leybodv <leybodv@gmail.com>
2022-06-13 20:35:13 +0300

    added logging

--- furnace/simple_temp_time_exporter.py
+++ furnace/simple_temp_time_exporter.py
@@ -2,11 +2,18 @@ from pathlib import Path
 
 from pycatalicism.furnace.exporter import Exporter
 from pycatalicism.furnace.furnace_data import FurnaceData
+import pycatalicism.furnace.furnace_logging as logging
 
 class SimpleTempTimeExporter(Exporter):
     """
     Class for exporting temperature vs. time data in a simple format defined in __str__ method of FurnaceData class.
     """
+
+    def __init__(self):
+        """
+        Registers logger with the class
+        """
+        self.logger = logging.get_logger(self.__class__.__name__)
 
     def export_data(self, data:FurnaceData, path:Path):
         """
@@ -19,5 +26,6 @@ class SimpleTempTimeExporter(Exporter):
         path:Path
             Path to file to export data to.
         """
+        self.logger.info(f'Exporting data to {path}')
         with path.open(mode='w') as f:
             f.write(str(data))
