997df596498d0a9ec7006463f07dfe2f91d3db80
leybodv <leybodv@gmail.com>
2022-06-13 18:34:57 +0300

    added docs

--- furnace/simple_temp_time_exporter.py
+++ furnace/simple_temp_time_exporter.py
@@ -5,10 +5,19 @@ from pycatalicism.furnace.furnace_data import FurnaceData
 
 class SimpleTempTimeExporter(Exporter):
     """
+    Class for exporting temperature vs. time data in a simple format defined in __str__ method of FurnaceData class.
     """
 
     def export_data(self, data:FurnaceData, path:Path):
         """
+        Exports temperature vs. time data in a format defined in __str__ method of FurnaceData class into file at path.
+
+        parameters
+        ----------
+        data:FurnaceData
+            Wrapper of temperature vs. time data
+        path:Path
+            Path to file to export data to.
         """
         with path.open(mode='w') as f:
             f.write(str(data))
