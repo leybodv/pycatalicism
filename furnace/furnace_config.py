31552f35debdc7159550364104a93a486c06841e
leybodv <leybodv@gmail.com>
2022-06-13 19:43:11 +0300

    added config parameters

--- furnace/furnace_config.py
+++ furnace/furnace_config.py
@@ -5,3 +5,23 @@ logging_levels = {
                 'SimpleTempTimeExporter'    :    logging.INFO,
                 'SimpleTempTimePlotter'     :    logging.INFO
                 }
+
+controller_type = ''
+port = ''
+baudrate = 
+bytesize = 
+parity = 
+stopbits = 
+timeout = 
+write_timeout = 
+rtscts = 
+address = 
+rsdl = 
+address_len = 
+
+plotter_type = ''
+fig_dpi = 96
+fig_width = 800 / fig_dpi
+fig_height = 600 / fig_dpi
+
+exporter_type = ''
