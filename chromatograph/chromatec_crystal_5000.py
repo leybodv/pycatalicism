14ba3e2de16efb3af94afda272179c25483303cf
leybodv <leybodv@gmail.com>
2022-07-30 22:18:12 +0300

    connect: added method

--- chromatograph/chromatec_crystal_5000.py
+++ chromatograph/chromatec_crystal_5000.py
@@ -1,5 +1,22 @@
+from pymodbus.client.sync import ModbusTcpClient
+
 from pycatalicism.chromatograph.chromatograph import Chromatograph
 
 class ChromatecCrystal5000(Chromatograph):
     """
     """
+
+    def __init__(self, control_panel_id:int, analytics_id:int):
+        """
+        """
+        self.control_panel_id = control_panel_id
+        self.analytics_id = analytics_id
+
+    def connect(self) -> bool:
+        """
+        """
+        # establish connection
+        self.modbus_client = ModbusTcpClient()
+        # start control panel
+        # start analytics
+        # check serial
