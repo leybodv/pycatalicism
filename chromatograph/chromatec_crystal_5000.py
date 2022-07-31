c2643bb22389e3a33eb26da33869b0734c47f877
leybodv <leybodv@gmail.com>
2022-08-01 00:32:44 +0300

    connect: adding implementation

--- chromatograph/chromatec_crystal_5000.py
+++ chromatograph/chromatec_crystal_5000.py
@@ -6,17 +6,20 @@ class ChromatecCrystal5000(Chromatograph):
     """
     """
 
-    def __init__(self, control_panel_id:int, analytics_id:int):
+    def __init__(self, control_panel_id:int, analytics_id:int, serial_id:str, chromatograph_command_address:int, application_command_address:int):
         """
         """
         self.control_panel_id = control_panel_id
         self.analytics_id = analytics_id
+        self.serial_id = serial_id
+        self.chromatograph_command_address = chromatograph_command_address
+        self.application_command_address = application_command_address
 
     def connect(self) -> bool:
         """
         """
-        # establish connection
         self.modbus_client = ModbusTcpClient()
-        # start control panel
-        # start analytics
+        self.modbus_client.write_registers(address=self.chromatograph_command_address, values=[1], unit=self.control_panel_id) # establish connection with chromatograph
+        self.modbus_client.write_registers(address=self.application_command_address, values=[1], unit=self.control_panel_id) # start control panel
+        self.modbus_client.write_registers(address=self.application_command_address, values=[5], unit=self.control_panel_id) # start analytics
         # check serial
