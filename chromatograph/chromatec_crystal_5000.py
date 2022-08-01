d531eaa1c186622eabde528ca1db231ef003ecf2
leybodv <leybodv@gmail.com>
2022-08-01 19:09:34 +0300

    _get_string: added method and implementation

--- chromatograph/chromatec_crystal_5000.py
+++ chromatograph/chromatec_crystal_5000.py
@@ -1,12 +1,13 @@
 from pymodbus.client.sync import ModbusTcpClient
 
 from pycatalicism.chromatograph.chromatograph import Chromatograph
+from pycatalicism.chromatograph.chromatograph_exceptions import ChromatographModbusException
 
 class ChromatecCrystal5000(Chromatograph):
     """
     """
 
-    def __init__(self, control_panel_id:int, analytics_id:int, serial_id:str, chromatograph_command_address:int, application_command_address:int):
+    def __init__(self, control_panel_id:int, analytics_id:int, serial_id:str, chromatograph_command_address:int, application_command_address:int, chromatograph_serial_id_address:int):
         """
         """
         self.control_panel_id = control_panel_id
@@ -14,6 +15,7 @@ class ChromatecCrystal5000(Chromatograph):
         self.serial_id = serial_id
         self.chromatograph_command_address = chromatograph_command_address
         self.application_command_address = application_command_address
+        self.chromatograph_serial_id_address = chromatograph_serial_id_address
 
     def connect(self) -> bool:
         """
@@ -22,5 +24,18 @@ class ChromatecCrystal5000(Chromatograph):
         self.modbus_client.write_registers(address=self.chromatograph_command_address, values=[1], unit=self.control_panel_id) # establish connection with chromatograph
         self.modbus_client.write_registers(address=self.application_command_address, values=[1], unit=self.control_panel_id) # start control panel
         self.modbus_client.write_registers(address=self.application_command_address, values=[5], unit=self.control_panel_id) # start analytics
-        serial_id = self._get_string()
+        serial_id = self._get_string(register_type='input', address=self.chromatograph_serial_id_address, count=15, unit=self.control_panel_id)
         return serial_id == self.serial_id
+
+    def _get_string(self, register_type:str, address:int, count:int, unit:int) -> str:
+        """
+        """
+        if register_type == 'input':
+            response = self.modbus_client.read_input_registers(address=address, count=count, unit=unit)
+            string = b''
+            for b in response.registers:
+                string += b.to_bytes(2, 'big')
+            string = string.decode().rstrip('\x00')
+        else:
+            raise ChromatographModbusException(f'Unknown type of register to read from: {register_type}')
+        return string
