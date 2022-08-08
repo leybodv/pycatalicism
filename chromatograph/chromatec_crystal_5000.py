cbc2694f7ad3638f8d681a2e5838a29d2529766f
leybodv <leybodv@gmail.com>
2022-08-08 22:19:32 +0300

    start_analysis: added implementation

--- chromatograph/chromatec_crystal_5000.py
+++ chromatograph/chromatec_crystal_5000.py
@@ -9,7 +9,7 @@ class ChromatecCrystal5000(Chromatograph):
     """
     """
 
-    def __init__(self, control_panel_id:int, analytics_id:int, serial_id:str, methods:dict[str,int], chromatograph_command_address:int, application_command_address:int, chromatograph_serial_id_address:int, set_method_address:int, current_step_address:int, connection_status_address:int, chromatogram_num_address:int):
+    def __init__(self, control_panel_id:int, analytics_id:int, serial_id:str, methods:dict[str,int], chromatograph_command_address:int, application_command_address:int, chromatograph_serial_id_address:int, set_method_address:int, current_step_address:int, connection_status_address:int, chromatogram_num_address:int, chromatogram_name_address:int, chromatogram_purpose_address:int, chromatogram_sample_volume_address:int, chromatogram_sample_dilution_address:int, chromatogram_operator_address:int, chromatogram_column_address:int):
         """
         """
         self.control_panel_id = control_panel_id
@@ -23,6 +23,12 @@ class ChromatecCrystal5000(Chromatograph):
         self.current_step_address = current_step_address
         self.connection_status_address = connection_status_address
         self.chromatogram_num_address = chromatogram_num_address
+        self.chromatogram_name_address = chromatogram_name_address
+        self.chromatogram_purpose_address = chromatogram_purpose_address
+        self.chromatogram_sample_volume_address = chromatogram_sample_volume_address
+        self.chromatogram_sample_dilution_address = chromatogram_sample_dilution_address
+        self.chromatogram_operator_address = chromatogram_operator_address
+        self.chromatogram_column_address = chromatogram_column_address
         self.modbus_client = None
 
     def connect(self) -> bool:
@@ -49,19 +55,29 @@ class ChromatecCrystal5000(Chromatograph):
             raise ChromatographModbusException('Chromatograph is not connected')
         self.modbus_client.write_registers(address=self.set_method_address, values=[self.methods[method]], unit=self.control_panel_id)
 
-    def start_analysis(self):
+    def start_analysis(self, chromatogram_name:str, chromatogram_purpose:int, chromatogram_sample_volume:float, chromatogram_sample_dilution:float, chromatogram_operator:str, chromatogram_column:str):
         """
         In order to start analysis we need either write 6 to chromatograph_command_address register, set method, wait until chromatograph is ready for analysis, write 6 to chromatograph_command_address again OR arrange register addresses such that set_method_address is lower than chromatograph_command_address
         """
         if not self.modbus_client:
             raise ChromatographModbusException('Chromatograph is not connected')
-        if self.is_ready_for_analysis():
-            self.modbus_client.write_registers(address=self.chromatograph_command_address, values=[4], unit=self.control_panel_id) # NB: check correctness of written value, maybe values=[6] is correct
-            # add information about sample
-        else:
-            # fire warning
-            pass
-        raise NotImplementedError()
+        while True:
+            if self.is_ready_for_analysis():
+                break
+            time.sleep(60)
+        self.modbus_client.write_registers(address=self.chromatograph_command_address, values=[6], unit=self.control_panel_id)
+        while True:
+            response = self.modbus_client.read_input_registers(address=self.current_step_address, count=2, unit=self.control_panel_id)
+            step = self._bytes_to_int(response.registers)
+            if step != 9:
+                break
+            time.sleep(60)
+        self.modbus_client.write_registers(address=self.chromatogram_name_address, values=self._string_to_bytes(chromatogram_name), unit=self.analytics_id)
+        self.modbus_client.write_registers(address=self.chromatogram_purpose_address, values=self._int_to_bytes(chromatogram_purpose), unit=self.analytics_id)
+        self.modbus_client.write_registers(address=self.chromatogram_sample_volume_address, values=self._double_to_bytes(chromatogram_sample_volume), unit=self.analytics_id)
+        self.modbus_client.write_registers(address=self.chromatogram_sample_dilution_address, values=self._double_to_bytes(chromatogram_sample_dilution), unit=self.analytics_id)
+        self.modbus_client.write_registers(address=self.chromatogram_operator_address, values=self._string_to_bytes(chromatogram_operator), unit=self.analytics_id)
+        self.modbus_client.write_registers(address=self.chromatogram_column_address, values=self._string_to_bytes(chromatogram_column), unit=self.analytics_id)
 
     def is_ready_for_analysis(self) -> bool:
         """
