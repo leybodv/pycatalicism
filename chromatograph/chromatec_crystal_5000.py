5205271fffdd66b6eb58cd24e4b25408267cc0b0
leybodv <leybodv@gmail.com>
2022-08-09 16:39:49 +0300

    connect: check lab_name instead of chromatogram num

--- chromatograph/chromatec_crystal_5000.py
+++ chromatograph/chromatec_crystal_5000.py
@@ -9,12 +9,13 @@ class ChromatecCrystal5000(Chromatograph):
     """
     """
 
-    def __init__(self, control_panel_id:int, analytics_id:int, serial_id:str, methods:dict[str,int], chromatograph_command_address:int, application_command_address:int, chromatograph_serial_id_address:int, set_method_address:int, current_step_address:int, connection_status_address:int, chromatogram_num_address:int, chromatogram_name_address:int, chromatogram_purpose_address:int, chromatogram_sample_volume_address:int, chromatogram_sample_dilution_address:int, chromatogram_operator_address:int, chromatogram_column_address:int):
+    def __init__(self, control_panel_id:int, analytics_id:int, serial_id:str, lab_name:str, methods:dict[str,int], chromatograph_command_address:int, application_command_address:int, chromatograph_serial_id_address:int, set_method_address:int, current_step_address:int, connection_status_address:int, chromatogram_lab_name_address:int, chromatogram_name_address:int, chromatogram_purpose_address:int, chromatogram_sample_volume_address:int, chromatogram_sample_dilution_address:int, chromatogram_operator_address:int, chromatogram_column_address:int):
         """
         """
         self.control_panel_id = control_panel_id
         self.analytics_id = analytics_id
         self.serial_id = serial_id
+        self.lab_name = lab_name
         self.methods = methods
         self.chromatograph_command_address = chromatograph_command_address
         self.application_command_address = application_command_address
@@ -22,7 +23,7 @@ class ChromatecCrystal5000(Chromatograph):
         self.set_method_address = set_method_address
         self.current_step_address = current_step_address
         self.connection_status_address = connection_status_address
-        self.chromatogram_num_address = chromatogram_num_address
+        self.chromatogram_lab_name_address = chromatogram_lab_name_address
         self.chromatogram_name_address = chromatogram_name_address
         self.chromatogram_purpose_address = chromatogram_purpose_address
         self.chromatogram_sample_volume_address = chromatogram_sample_volume_address
@@ -36,7 +37,7 @@ class ChromatecCrystal5000(Chromatograph):
         check some register from chromatograph analytics also!
         """
         self.modbus_client = ModbusTcpClient()
-        self.modbus_client.write_registers(address=self.application_command_address, values=[1], unit=self.control_panel_id) # start control panel
+        self.modbus_client.write_registers(address=self.application_command_address, values=[1], unit=self.control_panel_id)
         while True:
             response = self.modbus_client.read_input_registers(address=self.connection_status_address, count=1, unit=self.control_panel_id)
             if response.registers[0] == 7:
@@ -44,9 +45,9 @@ class ChromatecCrystal5000(Chromatograph):
             time.sleep(1)
         control_panel_response = self.modbus_client.read_input_registers(address=self.chromatograph_serial_id_address, count=15, unit=self.control_panel_id)
         serial_id = self._bytes_to_string(control_panel_response.registers)
-        analytics_response = self.modbus_client.read_input_registers(address=self.chromatogram_num_address, count=2, unit=self.analytics_id) # NB: check count!!!
-        self._bytes_to_int(analytics_response.registers) # will throw an exception if there is something wrong with modbus settings at analytics. Better to find another way to check this
-        return serial_id == self.serial_id
+        analytics_response = self.modbus_client.read_input_registers(address=self.chromatogram_lab_name_address, count=15, unit=self.analytics_id)
+        lab_name = self._bytes_to_string(analytics_response.registers)
+        return serial_id == self.serial_id and lab_name == self.lab_name
 
     def set_instrument_method(self, method:str):
         """
