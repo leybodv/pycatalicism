a5f95e397e18a15cfde8c3a3cb067df21512cf0b
leybodv <leybodv@gmail.com>
2022-08-11 20:44:28 +0300

    __init__: added method

--- chromatograph/chromatec_crystal_5000.py
+++ chromatograph/chromatec_crystal_5000.py
@@ -1,3 +1,14 @@
+import pycatalicism.chromatograph.chromatograph_logging as chromatograph_logging
+from pycatalicism.chromatograph.chromatec_control_panel_modbus import ChromatecControlPanelModbus
+from pycatalicism.chromatograph.chromatec_analytic_modbus import ChromatecAnalyticModbus
+
 class ChromatecCrystal5000():
     """
     """
+
+    def __init__(self, control_panel:ChromatecControlPanelModbus, analytic:ChromatecAnalyticModbus):
+        """
+        """
+        self._control_panel = control_panel
+        self._analytic = analytic
+        self._logger = chromatograph_logging.get_logger(self.__class__.__name__)
