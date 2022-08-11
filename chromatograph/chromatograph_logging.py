9ac5d422252e6c84912765e0b0e67b4b3c2b1ad1
leybodv <leybodv@gmail.com>
2022-08-11 13:51:37 +0300

    changed import statement

--- chromatograph/chromatograph_logging.py
+++ chromatograph/chromatograph_logging.py
@@ -4,7 +4,7 @@ Module for instantiating and configuring logger
 
 import logging
 
-from logging_config import logging_levels
+from pycatalicism.chromatograph.logging_config import logging_levels
 
 def get_logger(name:str) -> logging.Logger:
     """
