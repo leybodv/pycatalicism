ec548f73939dc760692b7e4efe8e79a1e30b5742
leybodv <leybodv@gmail.com>
2022-08-14 19:02:57 +0300

    added logging levels

--- chromatograph/logging_config.py
+++ chromatograph/logging_config.py
@@ -2,5 +2,8 @@ import logging
 
 # Dictionary of logging levels of corresponding classes
 logging_levels = {
-                'ChromatecControlPanelModbus'               :    5,
+                'ChromatecControlPanelModbus'               :		5,
+                'ChromatecAnalyticModbus'					:		5,
+                'ChromatecCrystal5000'						:		5,
+                'modbus_converter'							:		5,
                 }
