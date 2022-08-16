04cc61c20212f170025b0307186627ac50019135
leybodv <leybodv@gmail.com>
2022-08-16 20:08:20 +0300

    added furnace configs

--- config.py
+++ config.py
@@ -56,3 +56,47 @@ methods = {
         'zero'											:		9,
         'Водка-Маруся'									:		10,
         }
+
+## furnace configuration ##
+import serial
+
+# Dictionary of logging levels of corresponding classes
+logging_levels = {
+                'FurnaceData'               :    logging.INFO,
+                'Owen_TPM101_Controller'    :    logging.INFO,
+                'SimpleTempTimeExporter'    :    logging.INFO,
+                'SimpleTempTimePlotter'     :    logging.INFO
+                }
+
+# Furnace controller type
+controller_type = 'Owen_TPM101'
+# Furnace controller port name and corresponding port parameters (baudrate, bytesize, parity, stopbits) which must be the same as configured on controller device
+furnace_port = 'COM6'
+furnace_baudrate = 19200
+furnace_bytesize = serial.EIGHTBITS
+furnace_parity = serial.PARITY_NONE
+furnace_stopbits = serial.STOPBITS_ONE
+# Time in seconds to wait for the response from the device
+furnace_timeout = 0.1
+# Time in seconds to wait while message is sent to the device
+furnace_write_timeout = None
+# Enable/disable hardware flow control
+furnace_rtscts = False
+# Address of Owen TRM101 device
+furnace_address = 0
+# Time interval between message was sent to the device and controller was sent the answer in ms
+furnace_rsdl = 20
+# Address length in bits. Can be 8 or 11 bits, however only 8 bits is supported now
+furnace_address_len = 8
+
+# Type of plotter to plot temperature vs. time data
+plotter_type = 'simple_temp_time_plotter'
+# Resolution of figure
+fig_dpi = 96
+# Width of figure in inches
+fig_width = 800 / fig_dpi
+# Height of figure in inches
+fig_height = 600 / fig_dpi
+
+# Type of exporter to save temperature vs. time data
+exporter_type = 'simple_temp_time_exporter'
