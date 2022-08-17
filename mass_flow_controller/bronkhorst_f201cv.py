541d85b36e75fe3a4fa8bbb0661c578b38452af5
leybodv <leybodv@gmail.com>
2022-08-17 22:49:42 +0300

    connect: added implementation

--- mass_flow_controller/bronkhorst_f201cv.py
+++ mass_flow_controller/bronkhorst_f201cv.py
@@ -7,7 +7,10 @@ class BronkhorstF201CV():
     def connect(self):
         """
         """
-        raise NotImplementedError()
+        serial_response = self._propar_intrument.readParameter(dde_nr=92)
+        if not serial_response == self._serial:
+            raise MFCConnectionException(f'Wrong serial {serial_response} was received from the device {self._serial}')
+        self._connected = True
 
     def set_flow_rate(self):
         """
