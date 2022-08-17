1d2e8bf0cf26c64fa2c80629194e76dba12fab6c
leybodv <leybodv@gmail.com>
2022-08-17 22:19:20 +0300

    added methods

--- mass_flow_controller/bronkhorst_f201cv.py
+++ mass_flow_controller/bronkhorst_f201cv.py
@@ -1,3 +1,13 @@
 class BronkhorstF201CV():
     """
     """
+
+    def set_flow_rate(self):
+        """
+        """
+        raise NotImplementedError()
+
+    def set_calibration(self):
+        """
+        """
+        raise NotImplementedError()
