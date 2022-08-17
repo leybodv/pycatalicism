0e7e166e07ad1ce61c950c78f155fd9caf1c7095
leybodv <leybodv@gmail.com>
2022-08-17 22:53:18 +0300

    __init__: added implementation

--- mass_flow_controller/bronkhorst_f201cv.py
+++ mass_flow_controller/bronkhorst_f201cv.py
@@ -4,12 +4,19 @@ class BronkhorstF201CV():
     """
     """
 
+    def __init__(self, serial_address:str, serial_id:str):
+        """
+        """
+        self._propar_intrument = propar.instrument(comport=serial_address)
+        self._serial_id = serial_id
+        self._connected = False
+
     def connect(self):
         """
         """
-        serial_response = self._propar_intrument.readParameter(dde_nr=92)
-        if not serial_response == self._serial:
-            raise MFCConnectionException(f'Wrong serial {serial_response} was received from the device {self._serial}')
+        serial_id_response = self._propar_intrument.readParameter(dde_nr=92)
+        if not serial_id_response == self._serial_id:
+            raise MFCConnectionException(f'Wrong serial {serial_id_response} was received from the device {self._serial_id}')
         self._connected = True
 
     def set_flow_rate(self):
@@ -23,6 +30,11 @@ class BronkhorstF201CV():
         raise NotImplementedError()
 
     def get_flow_rate(self):
+        """
+        """
+        raise NotImplementedError()
+
+    def get_calibration(self):
         """
         """
         raise NotImplementedError()
