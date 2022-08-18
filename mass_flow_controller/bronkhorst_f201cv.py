e14a5d27cece1ddf77df256f45860fed0a8f240b
leybodv <leybodv@gmail.com>
2022-08-18 17:35:28 +0300

    set_flow_rate: added implementation

--- mass_flow_controller/bronkhorst_f201cv.py
+++ mass_flow_controller/bronkhorst_f201cv.py
@@ -6,24 +6,32 @@ class BronkhorstF201CV():
     """
     """
 
-    def __init__(self, serial_address:str, serial_id:str):
+    def __init__(self, serial_address:str, serial_id:str, calibrations:dict[int, BronkhorstMFCCalibration]):
         """
         """
-        self._propar_intrument = propar.instrument(comport=serial_address)
+        self._propar_instrument = propar.instrument(comport=serial_address)
         self._serial_id = serial_id
+        self._calibrations = calibrations
+        self._current_calibration = None
         self._connected = False
 
     def connect(self):
         """
         """
-        serial_id_response = self._propar_intrument.readParameter(dde_nr=92)
+        serial_id_response = self._propar_instrument.readParameter(dde_nr=92)
         if not serial_id_response == self._serial_id:
             raise MFCConnectionException(f'Wrong serial {serial_id_response} was received from the device {self._serial_id}')
+        self._current_calibration = self._propar_instrument.readParameter(dde_nr=24)
         self._connected = True
 
-    def set_flow_rate(self):
+    def set_flow_rate(self, flow_rate:float):
         """
         """
+        if not self._connected:
+            raise MFCStateException(f'Mass flow controller {self._serial_id} is not connected!')
+        percent_setpoint = flow_rate * 100 / self._calibrations[self._current_calibration].get_max_flow_rate()
+        propar_setpoint = int(percent_setpoint * 32000 / 100)
+        self._propar_instrument.setpoint = propar_setpoint
         raise NotImplementedError()
 
     def set_calibration(self):
