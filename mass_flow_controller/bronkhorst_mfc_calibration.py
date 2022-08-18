5c762903436ce037f6d4a7c368136f8680b0fdb5
leybodv <leybodv@gmail.com>
2022-08-18 17:40:13 +0300

    __init__: added implementation

--- mass_flow_controller/bronkhorst_mfc_calibration.py
+++ mass_flow_controller/bronkhorst_mfc_calibration.py
@@ -2,10 +2,13 @@ class BronkhorstMFCCalibration():
     """
     """
 
-    def __init__(self):
+    def __init__(self, max_flow_rate:float, gas:str, p_in:float, p_out:float):
         """
         """
-        raise NotImplementedError()
+        self._max_flow_rate = max_flow_rate
+        self._gas = gas
+        self._p_in = p_in
+        self._p_out = p_out
 
     def get_max_flow_rate(self):
         """
