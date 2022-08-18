7f8028c4f634f825f8873e59595bde02679d83e7
leybodv <leybodv@gmail.com>
2022-08-18 17:38:39 +0300

    added methods

--- mass_flow_controller/bronkhorst_mfc_calibration.py
+++ mass_flow_controller/bronkhorst_mfc_calibration.py
@@ -1,3 +1,13 @@
 class BronkhorstMFCCalibration():
     """
     """
+
+    def __init__(self):
+        """
+        """
+        raise NotImplementedError()
+
+    def get_max_flow_rate(self):
+        """
+        """
+        raise NotImplementedError()
