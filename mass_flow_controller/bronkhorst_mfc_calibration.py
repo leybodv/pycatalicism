7078621e8e3a08210a3cf38df3a268596d691dda
leybodv <leybodv@gmail.com>
2022-08-18 18:20:57 +0300

    __str__: added method

--- mass_flow_controller/bronkhorst_mfc_calibration.py
+++ mass_flow_controller/bronkhorst_mfc_calibration.py
@@ -10,6 +10,12 @@ class BronkhorstMFCCalibration():
         self._p_in = p_in
         self._p_out = p_out
 
+    def __str__(self) -> str:
+        """
+        """
+        string = f'[gas: {self._gas}, p_in: {self._p_in}, p_out: {self._p_out}, flow rate: {self._max_flow_rate} nml/min]'
+        return string
+
     def get_max_flow_rate(self) -> float:
         """
         """
