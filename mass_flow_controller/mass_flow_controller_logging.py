0541ba36ee6036595d1bfa1799dd495cd4153566
leybodv <leybodv@gmail.com>
2022-08-19 17:42:22 +0300

    add handler only if logger does not have one

--- mass_flow_controller/mass_flow_controller_logging.py
+++ mass_flow_controller/mass_flow_controller_logging.py
@@ -24,13 +24,11 @@ def get_logger(name:str) -> logging.Logger:
     logger.setLevel(logging_levels[name])
     logger.propagate = False
 
-    ch = logging.StreamHandler()
-    ch.setLevel(logging_levels[name])
-
-    formatter = logging.Formatter(fmt='[%(asctime)s] %(name)s.%(funcName)s: %(levelname)s: %(message)s', datefmt='%d.%m.%Y %H:%M:%S')
-
-    ch.setFormatter(formatter)
-
-    logger.addHandler(ch)
+    if not logger.hasHandlers():
+        ch = logging.StreamHandler()
+        ch.setLevel(logging_levels[name])
+        formatter = logging.Formatter(fmt='[%(asctime)s] %(name)s.%(funcName)s: %(levelname)s: %(message)s', datefmt='%d.%m.%Y %H:%M:%S')
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
 
     return logger
