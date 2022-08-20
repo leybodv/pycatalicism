690deaa96f94f4b3d19cc130f2998a5ab0e1a25c
leybodv <leybodv@gmail.com>
2022-08-20 18:18:13 +0300

    added methods

--- furnace/owen_tmp101.py
+++ furnace/owen_tmp101.py
@@ -1,3 +1,23 @@
 class OwenTPM101():
     """
     """
+
+    def __init__(self):
+        """
+        """
+        self._logger = furnace_logging.get_logger(self.__class__.__name__)
+
+    def connect(self):
+        """
+        """
+        raise NotImplementedError()
+
+    def set_temperature(self, temperature:float):
+        """
+        """
+        raise NotImplementedError()
+
+    def get_temperature(self) -> float:
+        """
+        """
+        raise NotImplementedError()
