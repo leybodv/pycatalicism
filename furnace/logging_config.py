a83976bc08b38fbce40610b713d5051713e35a62
leybodv <leybodv@gmail.com>
2022-08-21 18:20:04 +0300

    new file:   furnace/logging_config.py

---
+++ furnace/logging_config.py
@@ -0,0 +1,7 @@
+import logging
+
+# Dictionary of logging levels of corresponding classes
+logging_levels = {
+                'OwenProtocol'	:	5,
+                'OwenTPM101'	:	5,
+                }
