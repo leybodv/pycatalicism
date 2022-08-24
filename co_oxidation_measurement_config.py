024773d73e14d519906aa2d501f244f1ef628b59
leybodv <leybodv@gmail.com>
2022-08-24 15:49:12 +0300

    added config variables

---
+++ co_oxidation_measurement_config.py
@@ -0,0 +1,13 @@
+calibrations = [
+                0, # He mass flow controller
+                2, # CO2/O2 mass flow controller
+                4, # CH4/H2/CO mass flow controller
+                ]
+
+flow_rates = [
+            30, # He mass flow controller
+            4, # CO2/O2 mass flow controller
+            2, # CH4/H2/CO mass flow controller
+            ]
+
+temperatures = [] # activity measurement temperatures
