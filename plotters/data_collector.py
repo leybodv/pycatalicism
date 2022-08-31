bb9b857119a5cb340ba1085e7355deb525f8c078
leybodv <leybodv@gmail.com>
2022-08-31 17:11:21 +0300

    new file:   plotters/data_collector.py

---
+++ plotters/data_collector.py
@@ -0,0 +1,16 @@
+from multiprocessing import Process
+
+class DataCollector(Process):
+    """
+    """
+
+    def __init__(self):
+        """
+        """
+        super().__init__(daemon=False)
+        raise NotImplementedError()
+
+    def run(self):
+        """
+        """
+        raise NotImplementedError()
