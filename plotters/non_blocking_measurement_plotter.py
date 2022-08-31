85158ccbc470f150bb8b6b2e9a5278a7a8dce504
leybodv <leybodv@gmail.com>
2022-08-31 19:49:55 +0300

    new file:   plotters/non_blocking_measurement_plotter.py

---
+++ plotters/non_blocking_measurement_plotter.py
@@ -0,0 +1,20 @@
+import multiprocessing.connection
+
+class NonBlockingActivationPlotter():
+    """
+    """
+
+    def __init__(self):
+        """
+        """
+        raise NotImplementedError()
+
+    def __call__(self, pipe:multiprocessing.connection.Connection):
+        """
+        """
+        raise NotImplementedError()
+
+    def _call_back(self):
+        """
+        """
+        raise NotImplementedError()
