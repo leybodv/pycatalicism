23b34e02d2d149e7363405ea6bc762ec1872de09
Ilia Volk <ilia.volkov@outlook.com>
2022-09-07 22:36:15 +0300

    Move source code to src

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ setuptools.setup(
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/leybodv/pycatalicism",
-     packages=setuptools.find_packages(),
+     package_dir={'':"src"},
+     packages=setuptools.find_packages("src"),
      install_requires = install_requires,
      python_requires='>3.10.0',
      entry_points={
