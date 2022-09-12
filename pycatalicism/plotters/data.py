57bc20d46ec72640748261abcd0363d135fb3b2e
leybodv <leybodv@gmail.com>
2022-09-12 21:59:41 +0300

    add_point: added method

--- pycatalicism/plotters/data.py
+++ pycatalicism/plotters/data.py
@@ -1,3 +1,5 @@
+from pycatalicism.plotters.point import Point
+
 class Data():
     """
     """
@@ -6,3 +8,17 @@ class Data():
         """
         """
         self._label = label
+        self._x = []
+        self._y = []
+
+    def add_point(self, point:Point|None):
+        """
+        """
+        if point is None:
+            return
+        x = point.get_x()
+        y = point.get_y()
+        if x not in self._x:
+            self._x.append(x)
+            if y is not None:
+                self._y.append(y)
