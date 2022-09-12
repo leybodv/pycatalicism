d545c283d03de2d3f25e8e9b6bd2b2b7959ad0e3
leybodv <leybodv@gmail.com>
2022-09-12 22:01:21 +0300

    get_x, get_y: added methods

--- pycatalicism/plotters/point.py
+++ pycatalicism/plotters/point.py
@@ -8,3 +8,13 @@ class Point():
         self._x = x
         self._y = y
         self._label = label
+
+    def get_x(self) -> float:
+        """
+        """
+        return self._x
+
+    def get_y(self) -> float|None:
+        """
+        """
+        return self._y
