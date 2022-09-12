1ce23f598c00a32aba7a400f2529d7e4dfa20f5d
leybodv <leybodv@gmail.com>
2022-09-12 20:48:01 +0300

    __init__: added gases parameter

--- pycatalicism/plotters/process_plotter.py
+++ pycatalicism/plotters/process_plotter.py
@@ -6,6 +6,7 @@ import threading
 from pycatalicism.furnace.owen_tmp101 import OwenTPM101
 from pycatalicism.mass_flow_controller.bronkhorst_f201cv import BronkhorstF201CV
 from pycatalicism.chromatograph.chromatec_crystal_5000 import ChromatecCrystal5000
+from pycatalicism.chromatograph.chromatec_control_panel_modbus import WorkingStatus
 from pycatalicism.plotters.non_blocking_plotter import NonBlockingPlotter
 from pycatalicism.plotters.point import Point
 
@@ -14,7 +15,7 @@ class DataCollectorPlotter(threading.Thread):
     Class for plotting data from activation and activity measurement experiments.
     """
 
-    def __init__(self, furnace_controller:OwenTPM101, mass_flow_controllers:list[BronkhorstF201CV], chromatograph:ChromatecCrystal5000|None):
+    def __init__(self, furnace_controller:OwenTPM101, mass_flow_controllers:list[BronkhorstF201CV], gases:list[str], chromatograph:ChromatecCrystal5000|None):
         """
         Initialize base class, instance variables and start non blocking plotter in a separate process.
 
@@ -24,12 +25,15 @@ class DataCollectorPlotter(threading.Thread):
             furnace controller to get information about current temperature
         mass_flow_controllers:list[BronkhorstF201CV]
             list of mass flow controllers to get information about current flow rates
+        gases:list[str]
+            list of gases used for process, which will be added as legend to plot
         chromatograph:ChromatecCrystal5000
             chromatograph to get information about analysis start time
         """
         super().__init__(daemon=False)
         self._furnace_controller = furnace_controller
         self._mfcs = mass_flow_controllers
+        self._gases = gases
         self._chromatograph = chromatograph
         self._collector_pipe, self._plotter_pipe = multiprocessing.Pipe()
         self._plotter = NonBlockingPlotter()
@@ -54,7 +58,7 @@ class DataCollectorPlotter(threading.Thread):
         """
         self._running = False
 
-    def _collect_data(self) -> tuple[Point, Point, list[Point]]:
+    def _collect_data(self) -> tuple[Point, Point|None, list[Point]]:
         """
         """
         t = (time.time() - self._start_time) / 60.0
@@ -62,7 +66,7 @@ class DataCollectorPlotter(threading.Thread):
         temperature_point = Point(x=t, y=T, label='temperature')
         chromatograph_point = None
         if self._chromatograph is not None:
-            if self._chromatograph.get_status() is WorkingStatus.ANALYSIS:
+            if self._chromatograph.get_working_status() is WorkingStatus.ANALYSIS:
                 t = round((time.time() - self._start_time) / 60.0, 2) - self._chromatograph.get_analysis_time()
                 chromatograph_point = Point(x=t, y=None, label='chromatograms')
         flow_rate_points = []
