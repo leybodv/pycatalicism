43dda3a45b7e7eae30b0b022ede1aebdfc8857e9
leybodv <leybodv@gmail.com>
2022-09-12 20:40:43 +0300

    __init__: added method

--- pycatalicism/plotters/point.py
+++ pycatalicism/plotters/point.py
@@ -1,3 +1,10 @@
 class Point():
     """
     """
+
+    def __init__(self, x:float, y:float, label:str):
+        """
+        """
+        self._x = x
+        self._y = y
+        self._label = label
