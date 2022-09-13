cd266894c8bd1c0e8cad196ad980759bc94f8554
leybodv <leybodv@gmail.com>
2022-09-13 15:27:15 +0300

    get_analysis_time: renamed control_panel's get_analysis_time method

--- pycatalicism/chromatograph/chromatec_crystal_5000.py
+++ pycatalicism/chromatograph/chromatec_crystal_5000.py
@@ -200,8 +200,8 @@ class ChromatecCrystal5000():
             raise ChromatographStateException('Connect chromatograph first!')
         working_status = self._control_panel.get_current_working_status()
         if working_status is not WorkingStatus.ANALYSIS:
-            raise ChromatographStateException('Woring status is not analysis, cannot retreive analysis time')
+            raise ChromatographStateException('Working status is not analysis, cannot retreive analysis time')
         self._logger.info('Getting analysis time.')
-        analysis_time = self._control_panel.get_analysis_time()
+        analysis_time = self._control_panel.get_step_time()
         self._logger.info(f'{analysis_time = }')
         return analysis_time
