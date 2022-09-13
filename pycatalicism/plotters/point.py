12c828fb30636d2e889a75fa4d986b71d20f1c10
leybodv <leybodv@gmail.com>
2022-09-13 21:09:39 +0300

    added docs

--- pycatalicism/plotters/point.py
+++ pycatalicism/plotters/point.py
@@ -1,9 +1,20 @@
 class Point():
     """
+    Wrapper class to store 2D data point. Used by non-blocking plotter to send and receive data through multiprocessing pipe.
     """
 
     def __init__(self, x:float, y:float|None, label:str):
         """
+        Initialize instance variables.
+
+        parameters
+        ----------
+        x:float
+            x coordinate
+        y:float|None
+            y coordinate, None means that this is 1D data
+        label:str
+            label to associate with the data
         """
         self._x = x
         self._y = y
@@ -11,10 +22,22 @@ class Point():
 
     def get_x(self) -> float:
         """
+        Get x value.
+
+        returns
+        -------
+        x:float
+            x value
         """
         return self._x
 
     def get_y(self) -> float|None:
         """
+        Get y value.
+
+        returns
+        -------
+        y:float
+            y value
         """
         return self._y
