034f44b35c328a7e723e4063d99f340f7f8e8af4
leybodv <leybodv@gmail.com>
2022-09-14 10:03:39 +0300

    version up

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires = [
 
 setuptools.setup(
      name='pycatalicism',
-     version='1.0.0',
+     version='1.0.0.1',
      author="Denis Leybo",
      author_email="leybodv@gmail.com",
      description="Program controls catalytic activity of materials measurement equipment as well calculations",
