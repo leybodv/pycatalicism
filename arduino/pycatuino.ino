44c336e4db9f828d258f6a74be120e9bed4126bd
leybodv <leybodv@gmail.com>
2022-12-23 16:40:43 +0300

    finished sketch for valves controller

--- arduino/pycatuino.ino
+++ arduino/pycatuino.ino
@@ -1,6 +1,7 @@
 // pins for
 // valve #      1  2  3  4  5
 int valves[] = {2, 3, 4, 5, 6};
+int states[] = {LOW, LOW, LOW, LOW, LOW};
 
 void setup() {
   for (byte i = 0; i < 5; i = i + 1) {
@@ -11,17 +12,68 @@ void setup() {
 }
 
 void loop() {
-  int in_byte;
-  String in_str;
   if (Serial.available() > 0) {
-    in_byte = Serial.read();
-    if (in_byte != '@') {
+    String in_str = Serial.readStringUntil('#');
+    if (!in_str.startsWith("@")) {
       send_message_format_error();
+      return;
     }
-  in_str = Serial.readStringUntil('#');
-  dot_index = in_str.indexOf('.');
+    int dot1 = in_str.indexOf('.');
+    int dot2 = in_str.lastIndexOf('.');
+    if (dot1 == -1 || dot2 == -1 || dot1 == dot2) {
+      send_message_format_error();
+      return;
+    }
+    String command = in_str.substring(1, dot1);
+    String dev_num_str = in_str.substring(dot1+1, dot2);
+    long dev_num = dev_num_str.toInt();
+    if (dev_num < 1 || dev_num > 5) {
+      send_wrong_devnum_error();
+      return;
+    }
+    String value = in_str.substring(dot2+1);
+    if (command.equals("HSH")) {
+      if (!value.equals("NISMF")) {
+        send_wrong_handshake_error();
+        return;
+      } else {
+        Serial.write("@HSH.DBQWT#");
+      }
+    } else if (command.equals("GET")) {
+      if (states[dev_num-1] == LOW) {
+        Serial.write("@ANS.CLOSE#");
+      } else {
+        Serial.write("@ANS.OPEN#");
+      }
+    } else if (command.equals("SET")) {
+      if (value.equals("OPEN")) {
+        digitalWrite(valves[dev_num-1], HIGH);
+        states[dev_num-1] = HIGH;
+        Serial.write("@OK.OPEN#");
+      } else if (value.equals("CLOSE")) {
+        digitalWrite(valves[dev_num-1], LOW);
+        states[dev_num-1] = LOW;
+        Serial.write("@OK.CLOSE#");
+      } else {
+        send_wrong_value_error();
+        return;
+      }
+    }
   }
 }
 
 void send_message_format_error() {
+  Serial.write("@ERR.MSGFMT#");
+}
+
+void send_wrong_devnum_error() {
+  Serial.write("@ERR.DVNM#");
+}
+
+void send_wrong_handshake_error() {
+  Serial.write("@ERR.HNDSHK#");
+}
+
+void send_wrong_value_error() {
+  Serial.write("@ERR.VL#");
 }
