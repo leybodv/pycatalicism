de5ac86426e0c7aa2bdf80c30611540c0fb8c67b
leybodv <leybodv@gmail.com>
2022-12-23 18:29:36 +0300

    __init__: added method

--- pycatalicism/valves/arduino_valve_controller.py
+++ pycatalicism/valves/arduino_valve_controller.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import threading
 
 class ValveState(Enum):
     """
@@ -6,3 +7,35 @@ class ValveState(Enum):
     """
     CLOSE = 0
     OPEN = 1
+
+class ArduinoValveController():
+    """
+    """
+
+    def __init__(self, port:str, baudrate:int, bytesize:int, parity:str, stopbits:float, request_trials:int=3):
+        """
+        Initialize object with serial connection parameters. Register logger.
+
+        parameters
+        ----------
+        port:str
+            serial port for communication with controller
+        baudrate:int
+            baudrate to use for communication. Must be the same as in arduino sketch
+        bytesize:int
+            bytesize to use for communication. Must be the same as in arduino sketch
+        parity:str
+            parity to use for communication. Must be the same as in arduino sketch
+        stopbits:float
+            stopbits to use for communication. Must be the same as in arduino sketch
+        request_truals:int (default: 3)
+            how many times to try connecting to the controller before exception is thrown
+        """
+        self._port = port
+        self._baudrate = baudrate
+        self._bytesize = bytesize
+        self._parity = parity
+        self._stopbits = stopbits
+        self._read_write_lock = threading.Lock()
+        self._logger = valves_logging.get_logget(self.__class__.__name__)
+        self._connected = False
