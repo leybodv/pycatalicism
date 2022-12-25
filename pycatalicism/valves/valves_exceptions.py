a65735b7b942e2b694fe158f81c50ae327dba93e
leybodv <leybodv@gmail.com>
2022-12-25 11:37:00 +0300

    ControllerErrorException: added class

--- pycatalicism/valves/valves_exceptions.py
+++ pycatalicism/valves/valves_exceptions.py
@@ -2,3 +2,29 @@ class MessageValueException(Exception):
     """
     Exception is thrown when wrong value was got from the controller
     """
+
+class ControllerErrorException(Exception):
+    """
+    Exception is thrown when controller returns ERR message
+    """
+
+    def __init__(self, error_code:str):
+        """
+        Constructs exception with message specific to error code.
+
+        parameters
+        ----------
+        error_code:str
+            error code in accordance with connection protocol
+        """
+        if error_code == 'MSGFMT':
+            msg = 'Wrong message format'
+        elif error_code == 'DVNM':
+            msg = 'Wrong device numver'
+        elif error_code == 'HNDSHK':
+            msg = 'Wrong handshake word'
+        elif error_code == 'VL':
+            msg = 'Wrong value'
+        else:
+            msg = 'Unknown error code'
+        super().__init__(f'Controller returned error: {msg}')
