0a400f8fd58ed1d19cc7ffd05edc1a20cd53c089
leybodv <leybodv@gmail.com>
2022-12-30 13:20:54 +0300

    added support for multistep activation

--- pycatalicism/activation_config.py
+++ pycatalicism/activation_config.py
@@ -1,31 +1,73 @@
-# gases connected to mass flow controllers
+# gases connected to mass flow controllers, these will be shown on plot
 gases = [
         'He',
-        'O2',
+        'CO2',
         'H2',
         ]
+# valves states used at different steps
+valves = [
+          { # 1st step
+           'He'  : 'close',
+           'CO2' : 'close',
+           'H2'  : 'open',
+          },
+          { # 2nd step
+           'He'  : 'close',
+           'CO2' : 'close',
+           'H2'  : 'open',
+          },
+          { # 3rd step
+           'He'  : 'close',
+           'CO2' : 'close',
+           'H2'  : 'open',
+          },
+         ]
 # mass flow controllers calibrations
 # see config.py for details
 calibrations = [
-                0, # He mass flow controller
-                2, # CO2/O2 mass flow controller
-                2  # CH4/H2/CO mass flow controller
-                ]
+                [ # 1st step
+                 0, # He mass flow controller
+                 2, # CO2/O2 mass flow controller
+                 2,  # CH4/H2/CO mass flow controller
+                ],
+                [ # 2nd step
+                 0,
+                 2,
+                 2,
+                ],
+                [ # 3rd step
+                 0,
+                 2,
+                 2,
+                ],
+               ]
 # flow rates of gases during activation step
-activation_flow_rates = [
-                        0, # He mass flow controller
-                        0, # CO2/O2 mass flow controller
-                        36 # CH4/H2/CO mass flow controller
-                        ]
-# temperature of activation in °C
-activation_temperature = 300
-# activation duration in minutes
-activation_duration = 60
-# after the activation furnace is turned off and below this temperature gas flow rates will be changed to post_flow_rates values
-post_temperature = 100
-# flow rates of gases after the activation is over
-post_flow_rates = [
-                0, # He mass flow controller
-                0, # CO2/O2 mass flow controller
-                3  # CH4/H2/CO mass flow controller
-                ]
+flow_rates = [
+              [ # 1st step
+               0, # He mass flow controller
+               0, # CO2/O2 mass flow controller
+               36, # CH4/H2/CO mass flow controller
+              ],
+              [ # 2nd step
+               0,
+               0,
+               36,
+              ],
+              [ # 3rd step
+               0,
+               0,
+               3,
+              ],
+             ]
+# temperature of activation in °C (last step must be 0 to turn off heating)
+temperatures = [
+                0,   # 1st step
+                500, # 2nd step
+                0,   # 3rd step
+               ]
+# activation duration in minutes (last step must be None)
+times = [
+         30,   # 1st step
+         600,  # 2nd step
+         None, # 3rd step
+        ]
