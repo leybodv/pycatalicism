f5074ae0bcd316609b5860339e94a76c180a83ae
leybodv <leybodv@gmail.com>
2024-09-21 21:18:58 +0300

    __init__: added logging to file

--- pycatalicism/chromatograph/chromatec_analytic_modbus.py
+++ pycatalicism/chromatograph/chromatec_analytic_modbus.py
@@ -18,7 +18,7 @@ class ChromatecAnalyticModbus():
     Class represents modbus protocol for connection with chromatec analytic modbus slave
     """
 
-    def __init__(self, modbus_id:int, sample_name_holding_address:int, chromatogram_purpose_holding_address:int, sample_volume_holding_address:int, sample_dilution_holding_address:int, operator_holding_address:int, column_holding_address:int, lab_name_holding_address:int):
+    def __init__(self, modbus_id:int, sample_name_holding_address:int, chromatogram_purpose_holding_address:int, sample_volume_holding_address:int, sample_dilution_holding_address:int, operator_holding_address:int, column_holding_address:int, lab_name_holding_address:int, logfilename:str):
         """
         Initializes private instance variables. Registers logger.
 
@@ -40,6 +40,8 @@ class ChromatecAnalyticModbus():
             modbus address for column as written in the passport of chromatogram
         lab_name_holding_address:int
             modbus address for lab name as written in the passport of chromatogram
+        logfilename:str
+            path to file to log to
         """
         self._modbus_id = modbus_id
         self._sample_name_holding_address = sample_name_holding_address
@@ -51,7 +53,7 @@ class ChromatecAnalyticModbus():
         self._lab_name_holding_address = lab_name_holding_address
         self._modbus_client = ModbusTcpClient()
         self._read_write_lock = threading.Lock()
-        self._logger = chromatograph_logging.get_logger(self.__class__.__name__)
+        self._logger = chromatograph_logging.get_logger(self.__class__.__name__, logfilename)
 
     def set_sample_name(self, name:str):
         """
