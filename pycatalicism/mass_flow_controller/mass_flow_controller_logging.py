1afcd0e7acff1ed730df16099fd15e222061561c
leybodv <leybodv@gmail.com>
2024-09-21 21:29:58 +0300

    added logging to file

--- pycatalicism/mass_flow_controller/mass_flow_controller_logging.py
+++ pycatalicism/mass_flow_controller/mass_flow_controller_logging.py
@@ -3,10 +3,11 @@ Module for instantiating and configuring logger
 """
 
 import logging
+from pathlib import Path
 
 from pycatalicism.mass_flow_controller.logging_config import logging_levels
 
-def get_logger(name:str) -> logging.Logger:
+def get_logger(name:str, logfilename:str) -> logging.Logger:
     """
     Get logger with corresponding name configured to log to stdout.
 
@@ -14,6 +15,8 @@ def get_logger(name:str) -> logging.Logger:
     ----------
     name:str
         name of returned logger
+    logfilename:str
+        name of file to write logs to
 
     returns
     -------
@@ -24,11 +27,16 @@ def get_logger(name:str) -> logging.Logger:
     logger.setLevel(logging_levels[name])
     logger.propagate = False
 
-    if not logger.hasHandlers():
+    logfile = Path(logfilename).absolute()
+    logfile.parent.mkdir(parents=True, exist_ok=True)
+    if not logger.handlers:
         ch = logging.StreamHandler()
+        fh = logging.FileHandler(filename=logfile)
         ch.setLevel(logging_levels[name])
+        fh.setLevel(logging_levels[name])
         formatter = logging.Formatter(fmt='[%(asctime)s] %(name)s.%(funcName)s: %(levelname)s: %(message)s', datefmt='%d.%m.%Y %H:%M:%S')
         ch.setFormatter(formatter)
+        fh.setFormatter(formatter)
         logger.addHandler(ch)
-
+        logger.addHandler(fh)
     return logger
